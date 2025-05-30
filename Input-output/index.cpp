813efde17dc8d8d09dd9518addc5de1b6d7b6215
helper.dev <basakayush942@gmail.com>
2025-05-30 12:32:43 +0530

    input and output of cpp added

---
+++ Input-output/index.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter a number: ";           // Prompt the user
+    cin >> x;                             // Read input
+    cout << "Number you have given: " << x << endl; // Print with spacing and newline
+    return 0;
+}
